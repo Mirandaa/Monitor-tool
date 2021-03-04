2e829a8a734df74886a757952fa89f8eeb15f193
邓荟丹 <denghuidan@denghuidandeMacBook-Pro.local>
2021-03-04 17:04:07 +0800

    force graph example

--- src/views/applications/Applications.js
+++ src/views/applications/Applications.js
@@ -1,53 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import ReactECharts from 'echarts-for-react';
-import { getExampleData } from '../../api';
+import React, { useState, useEffect } from "react";
+import ReactECharts from "echarts-for-react";
+import { getExampleData } from "../../api";
+import data from './example-data';
+
 const Page = () => {
   const [option, setOption] = useState({});
   useEffect(() => {
     getExampleData().then((graph) => {
+      graph = data;
       setOption({
         tooltip: {},
-        legend: [{
-          data: graph.categories.map(function (a) {
-            return a.name;
-          })
-        }],
+        legend: [
+          {
+            data: graph.categories.map(function (a) {
+              return a.name;
+            }),
+          },
+        ],
         series: [
           {
-            name: 'Les Miserables',
-            type: 'graph',
-            layout: 'none',
+            name: "Les Miserables",
+            type: "graph",
+            layout: "force",
             data: graph.nodes,
             links: graph.links,
             categories: graph.categories,
             roam: true,
             label: {
               show: true,
-              position: 'right',
-              formatter: '{b}'
+              position: "right",
+              formatter: "{b}",
             },
             labelLayout: {
-              hideOverlap: true
+              hideOverlap: true,
             },
             scaleLimit: {
               min: 0.4,
-              max: 2
+              max: 2,
             },
             lineStyle: {
-              color: 'source',
-              curveness: 0.3
+              color: "source",
+              curveness: 0.3,
+            },
+            force: {
+              repulsion: 300
             }
-          }
+          },
         ]
-      })
-    })
+      });
+    });
   }, []);
 
+  const onClick = (e) => {
+    console.log(e);
+  };
 
-  return <ReactECharts
-    option={option}
-    style={{ height: '700px', width: '100%' }}
-  />;
+  const onEvents = {
+    'click': (e) => {
+      console.log(123, e);
+    }
+  };
+
+  return (
+    <ReactECharts
+      option={option}
+      style={{ height: "700px", width: "100%" }}
+      onEvents={onEvents}
+    />
+  );
 };
 
 export default Page;
